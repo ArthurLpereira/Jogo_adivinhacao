7db7147c68e6f47adcf05f501177b20e4214b319
Arthur Leandro Pereira <145617892+ArthurLpereira@users.noreply.github.com>
2024-10-01 13:46:37 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let select = document.getElementById("escolher_dificuldade");
 let button = document.getElementById("confirmar_dificuldade");
+let parte_baixo = document.getElementById("parte_baixo")
 button.addEventListener("click", () =>{
     let num_colocar;
-
     if (select.value === 'facil') {
         num_colocar = 20;
     } else if (select.value === 'medio') {
@@ -14,6 +14,12 @@ button.addEventListener("click", () =>{
         return;
     }
     console.log(num_colocar)
-    window.location.href = "./adivinhe.html";
-})
+    window.location.href = "./adivinha.html";
+    // if(parte_baixo.style.display === "none"){
+    //     parte_baixo.style.display ="block";
+    // }
+});
+
+
+
 
--- js/scriptVS.js
+++ js/scriptVS.js
@@ -1,8 +1,7 @@
 const readline = require('readline-sync');
-continuar = 0
+continuar = 0;
 
 // Função para selecionar a dificuldade (no vs)
-// JÁ FOI FEITO
 function dificuldade() {
     var resposta = readline.question("\nDigite o nivel de dificuldade: [facil] [medio] [dificil]: ");
     var num_colocar;
@@ -18,64 +17,63 @@ function dificuldade() {
     }
 }
 
-//Função do jogo
-function game(num_colocar) {
-    console.log("O numero está entre 1 e " + num_colocar + "\n Voce tem 6 tentativas");
+// Função do jogo
+function game(num_colocar, tentativas) {
+    console.log("O número está entre 1 e " + num_colocar + "\nVocê tem " + tentativas + " tentativas.");
     var numaleatorio = Math.floor(Math.random() * num_colocar) + 1;
-    var tentativas = 0;
     var num;
     let pontuacao = pontuacao_inicial;
 
-    for (tentativas = 1; tentativas <= 6; tentativas++) {
-        num = readline.question("Tentativa " + tentativas + ": Escolha um numero: ");
+    for (let tentativaAtual = 1; tentativaAtual <= tentativas; tentativaAtual++) {
+        num = readline.question("Tentativa " + tentativaAtual + ": Escolha um número: ");
         if (parseInt(num) === numaleatorio) {
-            console.log("Acertou o numero!");
+            console.log("Acertou o número!");
             break;
         } else if (parseInt(num) < numaleatorio) {
-            console.log("O numero e maior.");
+            console.log("O número é maior.");
         } else {
-            console.log("O numero e menor.");
+            console.log("O número é menor.");
         }
         pontuacao -= reducao;
     }
 
     if (tentativas > 6) {
-        console.log("Voce excedeu o numero de tentativas. O numero era " + numaleatorio + ".");
-        console.log("\nO número era " + numaleatorio);
+        console.log("Você excedeu o número de tentativas. O número era " + numaleatorio + ".");
         pontuacao = 0;
-    } else {
-        console.log("Voce tentou: " + tentativas + " vezes.");
-
     }
-    console.log("A sua pontuacao foi " + pontuacao)
+    console.log("Sua pontuação foi: " + pontuacao);
 }
 
 // Loop do jogo
 do {
     console.log("Bem-vindo ao jogo");
-    console.log("\nRegras: \n\nVocê deve escolher o nivel de dificuldade; \n\nLogo apos, um número aleatorio sera gerado;\n\nAs tentativas serao limitadas;\n\nVoce comeca com 100 pontos, a cada tentativa errada perde 10.")
+    console.log("\nRegras: \n\nVocê deve escolher o nível de dificuldade; \n\nLogo após, um número aleatório será gerado;\n\nAs tentativas serão limitadas;\n\nVocê começa com 100 pontos, a cada tentativa errada perde 10.");
+    
     var respostaUsuario = dificuldade();
+    var reducao = 10;
+    var pontuacao_inicial = 100;
+    var tentativas;  // Declaração da variável `tentativas` fora do switch
+
     switch (respostaUsuario) {
-        //Modo facil
+        // Modo fácil
         case 20:
-            console.log(game(20))
-            var reducao = 10
-            var pontuacao_inicial = 5;  
+            tentativas = 10;  // Definindo o número de tentativas para o modo fácil
+            game(20, tentativas);
             break;
+        // Modo médio
         case 50:
-            //Modo medio
-            console.log(game(50))
-            var reducao = 10
-            var pontuacao_inicial = 5;  
+            tentativas = 6;  // Definindo o número de tentativas para o modo médio
+            game(50, tentativas);
             break;
+        // Modo difícil
         case 200:
-            //Modo dificil
-            console.log(game(200))
-            var reducao = 10
-            var pontuacao_inicial = 5;  
+            tentativas = 3;  // Definindo o número de tentativas para o modo difícil
+            game(200, tentativas);
             break;
         default:
+            console.log("Dificuldade inválida!");
             break;
     }
-    continuar = readline.question("Deseja continuar ? Se quiser digite 'sim' ")
-} while (continuar.toLowerCase() == "sim")
+    
+    continuar = readline.question("Deseja continuar? Se quiser, digite 'sim': ");
+} while (continuar.toLowerCase() == "sim");
